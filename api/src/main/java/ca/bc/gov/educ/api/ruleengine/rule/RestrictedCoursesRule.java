5067e22a93c15a69ebfe2fa2686a64a723e86ce3
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-02-08 18:29:53 -0800

    Code Smell Removed

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/RestrictedCoursesRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/RestrictedCoursesRule.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.educ.api.ruleengine.rule;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -36,31 +37,32 @@ public class RestrictedCoursesRule implements Rule {
         List<StudentCourse> studentCourses = RuleProcessorRuleUtils.getUniqueStudentCourses(
                 ruleProcessorData.getStudentCourses(), ruleProcessorData.isProjected());
         List<CourseRestriction> restrictedCourses = ruleProcessorData.getCourseRestrictions();
-        if(restrictedCourses != null && !restrictedCourses.isEmpty()) {
-			for (int i = 0; i < studentCourses.size(); i++) {
-				StudentCourse sCourse = studentCourses.get(i);
-				String courseCode = studentCourses.get(i).getCourseCode();
-				String courseLevel = studentCourses.get(i).getCourseLevel();
-				List<CourseRestriction> shortenedList = getMinimizedRestrictedCourses(restrictedCourses, courseLevel, courseCode);
+		if(restrictedCourses == null) {
+			restrictedCourses = new ArrayList<>();
+		}
+       	for (int i = 0; i < studentCourses.size(); i++) {
+			StudentCourse sCourse = studentCourses.get(i);
+			String courseCode = studentCourses.get(i).getCourseCode();
+			String courseLevel = studentCourses.get(i).getCourseLevel();
+			List<CourseRestriction> shortenedList = getMinimizedRestrictedCourses(restrictedCourses, courseLevel, courseCode);
 
-				if (!shortenedList.isEmpty()) {
-					for (CourseRestriction courseRestriction : shortenedList) {
-						String restrictedCourse = courseRestriction.getRestrictedCourse();
-						StudentCourse tempCourseRestriction = studentCourses.stream()
-								.filter(sc -> restrictedCourse.compareTo(sc.getCourseCode()) == 0)
-								.findAny()
-								.orElse(null);
-						if (tempCourseRestriction != null
-								&& !tempCourseRestriction.isRestricted()
-								&& !sCourse.isRestricted()
-								&& !courseCode.equalsIgnoreCase(restrictedCourse)) {
+			if (!shortenedList.isEmpty()) {
+				for (CourseRestriction courseRestriction : shortenedList) {
+					String restrictedCourse = courseRestriction.getRestrictedCourse();
+					StudentCourse tempCourseRestriction = studentCourses.stream()
+							.filter(sc -> restrictedCourse.compareTo(sc.getCourseCode()) == 0)
+							.findAny()
+							.orElse(null);
+					if (tempCourseRestriction != null
+							&& !tempCourseRestriction.isRestricted()
+							&& !sCourse.isRestricted()
+							&& !courseCode.equalsIgnoreCase(restrictedCourse)) {
 
-							compareCredits(sCourse, tempCourseRestriction, studentCourses, i);
-						}
+						compareCredits(sCourse, tempCourseRestriction, studentCourses, i);
 					}
 				}
-
 			}
+
 		}
         ruleProcessorData.setStudentCourses(studentCourses);
 		List<StudentCourse> excludedCourses = RuleProcessorRuleUtils.getExcludedStudentCourses(ruleProcessorData.getStudentCourses(), ruleProcessorData.isProjected());
