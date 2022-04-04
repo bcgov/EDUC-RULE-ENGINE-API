a074a50f3bfde3692e1c29da5f1a2ae13f9583ff
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-04-04 09:01:07 -0700

    GRAD2-1087

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/service/RuleEngineService.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/service/RuleEngineService.java
@@ -1,18 +1,14 @@
 package ca.bc.gov.educ.api.ruleengine.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 import ca.bc.gov.educ.api.ruleengine.dto.ProgramAlgorithmRule;
 import ca.bc.gov.educ.api.ruleengine.dto.RuleProcessorData;
 import ca.bc.gov.educ.api.ruleengine.rule.Rule;
 import ca.bc.gov.educ.api.ruleengine.rule.RuleFactory;
 import ca.bc.gov.educ.api.ruleengine.util.RuleProcessorRuleUtils;
 import lombok.SneakyThrows;
-
-import java.io.FileWriter;
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 @Service
 public class RuleEngineService {
@@ -32,17 +28,6 @@ public class RuleEngineService {
         
         RuleProcessorData originalData = RuleProcessorRuleUtils.cloneObject(ruleProcessorData);
         ruleProcessorData.setGraduated(true);
-
-        try {
-            String ruleData = new ObjectMapper().writeValueAsString(originalData);
-            FileWriter myWriter = new FileWriter("filename.txt");
-            myWriter.write(ruleData);
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
         for (ProgramAlgorithmRule gradAlgorithmRule : originalData.getAlgorithmRules()) {
         	Rule rule = RuleFactory.createRule(gradAlgorithmRule.getAlgorithmRuleCode().getRuleImplementation(), ruleProcessorData);
             rule.setInputData(ruleProcessorData);
