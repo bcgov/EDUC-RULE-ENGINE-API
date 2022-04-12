5fd31d3737afe16a75827346c9c65da9304d9ed4
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-04-12 11:00:11 -0700

    GRAD2-1129 bug fix

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/DuplicateCoursesRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/DuplicateCoursesRule.java
@@ -31,7 +31,7 @@ public class DuplicateCoursesRule implements Rule {
     public RuleData fire() {
 
         logger.info("###################### Finding DUPLICATE courses ######################");
-        List<StudentCourse> studentCourseList = ruleProcessorData.getStudentCourses();
+        List<StudentCourse> studentCourseList = RuleProcessorRuleUtils.getUniqueStudentCourses(ruleProcessorData.getStudentCourses(),ruleProcessorData.isProjected());
 
         for (int i = 0; i < studentCourseList.size() - 1; i++) {
 
