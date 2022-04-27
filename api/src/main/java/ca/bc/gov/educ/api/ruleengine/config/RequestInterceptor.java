d6866f1cb71f187ea903bb6f7a50a41326b0d861
Jinil Sung <infstar@gmail.com>
2022-04-27 13:19:24 -0700

    Update RequestInterceptor.java
    AsyncInterceptor is used to capture http request.

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/config/RequestInterceptor.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/config/RequestInterceptor.java
@@ -6,14 +6,14 @@ import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.Instant;
 
 @Component
-public class RequestInterceptor implements HandlerInterceptor {
+public class RequestInterceptor implements AsyncHandlerInterceptor {
 
 	@Autowired
 	RuleEngineApiConstants constants;
