4e8ee8b80bda7d0c40b4aa3748121a0043b5f014
Jinil Sung <infstar@gmail.com>
2022-12-08 11:52:47 -0800

    Update EquivalencyRule.java
    Clean up unused imports.

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/EquivalencyRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/EquivalencyRule.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
