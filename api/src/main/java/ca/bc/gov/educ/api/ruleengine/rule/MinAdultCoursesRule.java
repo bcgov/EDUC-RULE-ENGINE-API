5cc4f615beab6826865cec4e4e4295497ad06ee1
Kamal Mohammed <kamal.mohammed@outlook.com>
2022-12-12 09:56:57 -0700

    GRAD2-1745 - Adult Rule: Min Adult Courses 18/19 Rule - modify default DD to 01 for both and remove "equals"

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/MinAdultCoursesRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/MinAdultCoursesRule.java
@@ -85,11 +85,16 @@ public class MinAdultCoursesRule implements Rule {
 					logger.debug(e.getMessage());
 				}
 				//Change DOB to first of the month for calculation
-				int age = calculateAge(dobOfStudent.substring(0, 8).concat("01"), RuleEngineApiUtils.formatDate(temp, "yyyy-MM-dd"));
-				if(age >= 18 && (totalCredits + sc.getCredits()) <= requiredCredits) {
-						totalCredits += sc.getCredits();
-						AlgorithmSupportRule.setGradReqMet(sc,gradProgramRule);
-						sc.setUsed(true);
+				Period agePeriod = calculateAge(dobOfStudent.substring(0, 8).concat("01"), RuleEngineApiUtils.formatDate(temp, "yyyy-MM-dd"));
+
+				int years = agePeriod.getYears();
+				int months = agePeriod.getMonths();
+
+				if( (years > 18 || (years == 18 && months > 0 ) )
+						&& (totalCredits + sc.getCredits()) <= requiredCredits) {
+					totalCredits += sc.getCredits();
+					AlgorithmSupportRule.setGradReqMet(sc,gradProgramRule);
+					sc.setUsed(true);
 				}
 				if (totalCredits == requiredCredits) {
 					break;
@@ -136,11 +141,11 @@ public class MinAdultCoursesRule implements Rule {
 		return ruleProcessorData;
 	}
 	
-	public int calculateAge(String dob,String sessionDate) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	public Period calculateAge(String dob, String sessionDate) {
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate birthDate = LocalDate.parse(dob, dateFormatter);
         LocalDate sDate = LocalDate.parse(sessionDate, dateFormatter);
-        return Period.between(birthDate, sDate).getYears();
+        return Period.between(birthDate, sDate);
     }
 
 	@Override
