3925356009f60654a36b34927a1d4a120c217a7f
alexryba <alex.rybakov@gov.bc.ca>
2023-04-24 11:25:00 -0600

    Timezone config fix

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/config/GradRuleEngineApiConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/config/GradRuleEngineApiConfig.java
@@ -1,10 +1,14 @@
 package ca.bc.gov.educ.api.ruleengine.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.TimeZone;
 
 @Configuration
 public class GradRuleEngineApiConfig {
@@ -17,6 +21,19 @@ public class GradRuleEngineApiConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false)
+                // Set timezone for JSON serialization as system timezone
+                .timeZone(TimeZone.getDefault())
+                .build();
+    }
+
+    @Bean
+    Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
     }
 
 }
