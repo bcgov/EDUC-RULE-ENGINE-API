b6fed59730dab788b0f1bd7652b374e2c4d4f7cc
Jinil Sung <infstar@gmail.com>
2024-03-07 15:12:58 -0800

    GRAD2-2503: task is complete.
    GRAD2-2503: task is complete.

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/AssessmentRegistrationsRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/AssessmentRegistrationsRule.java
@@ -9,13 +9,12 @@ import ca.bc.gov.educ.api.ruleengine.util.RuleProcessorRuleUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.text.ParseException;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -36,38 +35,9 @@ public class AssessmentRegistrationsRule implements Rule {
 		List<StudentAssessment> studentAssessmentList =  RuleProcessorRuleUtils.getUniqueStudentAssessments(ruleProcessorData.getStudentAssessments(),ruleProcessorData.isProjected());
 
 		for (StudentAssessment studentAssessment : studentAssessmentList) {
-			String today = RuleEngineApiUtils.formatDate(new Date(), "yyyy-MM-dd");
-			String sessionDate = studentAssessment.getSessionDate() + "/01";
-			try {
-				Date temp = RuleEngineApiUtils.parseDate(sessionDate, "yyyy/MM/dd");
-				sessionDate = RuleEngineApiUtils.formatDate(temp, "yyyy-MM-dd");
-			} catch (ParseException pe) {
-				logger.error("ERROR: {}", pe.getMessage());
-			}
-
-			int diff = RuleEngineApiUtils.getDifferenceInMonths(sessionDate, today);
-			String proficiencyScore;
-			if (studentAssessment.getProficiencyScore() == null) {
-				proficiencyScore = "0.0";
-			} else {
-				proficiencyScore = studentAssessment.getProficiencyScore().toString();
-			}
-			String specialCase;
-			if (studentAssessment.getSpecialCase() == null) {
-				specialCase = "";
-			} else {
-				specialCase = studentAssessment.getSpecialCase();
-			}
-			String exceededWriteFlag;
-            if(studentAssessment.getExceededWriteFlag() == null) {
-            	exceededWriteFlag = "";
-            }else {
-            	exceededWriteFlag = studentAssessment.getExceededWriteFlag();
-            }
-	            
-			if ("".compareTo(specialCase.trim()) == 0
-					&& "Y".compareTo(exceededWriteFlag.trim()) != 0
-					&& "0.0".compareTo(proficiencyScore) == 0 && diff <= 0) {
+			String specialCase = StringUtils.isBlank(studentAssessment.getSpecialCase())? "" : studentAssessment.getSpecialCase();
+			String wroteFlag = StringUtils.isBlank(studentAssessment.getWroteFlag())? "" : studentAssessment.getWroteFlag();
+			if ("".compareTo(specialCase.trim()) == 0 && "".compareTo(wroteFlag.trim()) == 0) {
 				studentAssessment.setProjected(true);
 			}
 		}
--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/IncompleteAssessmentsRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/IncompleteAssessmentsRule.java
@@ -3,18 +3,16 @@ package ca.bc.gov.educ.api.ruleengine.rule;
 import ca.bc.gov.educ.api.ruleengine.dto.RuleData;
 import ca.bc.gov.educ.api.ruleengine.dto.RuleProcessorData;
 import ca.bc.gov.educ.api.ruleengine.dto.StudentAssessment;
-import ca.bc.gov.educ.api.ruleengine.util.RuleEngineApiUtils;
 import ca.bc.gov.educ.api.ruleengine.util.RuleProcessorRuleUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.text.ParseException;
-import java.util.Date;
 import java.util.List;
 
 @Data
@@ -32,40 +30,11 @@ public class IncompleteAssessmentsRule implements Rule {
         List<StudentAssessment> studentAssessmentList =  RuleProcessorRuleUtils.getUniqueStudentAssessments(ruleProcessorData.getStudentAssessments(),ruleProcessorData.isProjected());
 
          for (StudentAssessment studentAssessment : studentAssessmentList) {
-            String today = RuleEngineApiUtils.formatDate(new Date(), "yyyy-MM-dd");
-            String sessionDate = studentAssessment.getSessionDate() + "/01";
-            try {
-                Date temp = RuleEngineApiUtils.parseDate(sessionDate, "yyyy/MM/dd");
-                sessionDate = RuleEngineApiUtils.formatDate(temp, "yyyy-MM-dd");
-            } catch (ParseException pe) {
-                logger.error("ERROR: {}", pe.getMessage());
-            }
-
-            int diff = RuleEngineApiUtils.getDifferenceInMonths(sessionDate,today);
-            String proficiencyScore;
-            if(studentAssessment.getProficiencyScore() == null) {
-            	proficiencyScore = "0.0";
-            }else {
-            	proficiencyScore = studentAssessment.getProficiencyScore().toString();
-            }
-            String specialCase;
-            if(studentAssessment.getSpecialCase() == null) {
-            	specialCase = "";
-            }else {
-            	specialCase = studentAssessment.getSpecialCase();
-            }
-            String exceededWriteFlag;
-            if(studentAssessment.getExceededWriteFlag() == null) {
-            	exceededWriteFlag = "";
-            }else {
-            	exceededWriteFlag = studentAssessment.getExceededWriteFlag();
-            }
-            if ("".compareTo(specialCase.trim()) == 0 
-            		&& "Y".compareTo(exceededWriteFlag.trim()) != 0
-            		&& "0.0".compareTo(proficiencyScore) == 0
-                    && diff > 0) {
-            	studentAssessment.setNotCompleted(true);
-            }
+             String specialCase = StringUtils.isBlank(studentAssessment.getSpecialCase())? "" : studentAssessment.getSpecialCase();
+             String wroteFlag = StringUtils.isBlank(studentAssessment.getWroteFlag())? "" : studentAssessment.getWroteFlag();
+             if ("".compareTo(specialCase.trim()) == 0 && "N".compareTo(wroteFlag.trim()) == 0) {
+                 studentAssessment.setNotCompleted(true);
+             }
         }
 
         ruleProcessorData.setExcludedAssessments(RuleProcessorRuleUtils.maintainExcludedAssessments(studentAssessmentList,ruleProcessorData.getExcludedAssessments(),ruleProcessorData.isProjected()));
