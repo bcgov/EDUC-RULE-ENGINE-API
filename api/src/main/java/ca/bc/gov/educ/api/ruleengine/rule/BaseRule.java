a8325aa9e656f97dfb3d5b1fd17746e974f7dcee
arybakov <alex.rybakov@cgi.com>
2024-06-27 09:03:29 -0600

    GRAD2-2799-P3 GRAD Incident: REGALG is creating SCCP certificates for students with future completion dates- edit summary P3 GRAD Incident: REGALG is creating SCCP certificates for students with future completion dates

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/BaseRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/BaseRule.java
@@ -6,10 +6,13 @@ import java.util.Date;
 public abstract class BaseRule implements Rule {
 
     Date toLastDayOfMonth(Date date) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
-        return cal.getTime();
+        if(date != null) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+            return cal.getTime();
+        }
+        return null;
     }
 
 }
