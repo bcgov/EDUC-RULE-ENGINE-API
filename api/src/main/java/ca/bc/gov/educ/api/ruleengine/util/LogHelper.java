03fb530588e5a28d07e3dcd5ca228edc029fe0a6
Kamal Mohammed <kamal.mohammed@outlook.com>
2025-05-13 05:06:44 -0600

    GRAD2-3450 - IAC for Program, Course and Rule Engine APIs

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/util/LogHelper.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/util/LogHelper.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
+import org.springframework.http.HttpMethod;
 import org.springframework.lang.NonNull;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -44,6 +46,35 @@ public final class LogHelper {
       httpMap.put("server_http_request_payload", String.valueOf(request.getAttribute("payload")));
       httpMap.put("server_http_request_remote_address", request.getRemoteAddr());
       httpMap.put("server_http_request_client_name", StringUtils.trimToEmpty(request.getHeader("X-Client-Name")));
+      MDC.putCloseable("httpEvent", mapper.writeValueAsString(httpMap));
+      log.info("");
+      MDC.clear();
+    } catch (final Exception exception) {
+      log.error(EXCEPTION, exception);
+    }
+  }
+
+  /**
+   * WebClient to call other REST APIs
+   *
+   * @param method
+   * @param url
+   * @param responseCode
+   * @param correlationID
+   */
+  public static void logClientHttpReqResponseDetails(@NonNull final HttpMethod method, final String url, final int responseCode, final List<String> correlationID, final List<String> requestSource, final boolean logging) {
+    if (!logging) return;
+    try {
+      final Map<String, Object> httpMap = new HashMap<>();
+      httpMap.put("client_http_response_code", responseCode);
+      httpMap.put("client_http_request_method", method.toString());
+      httpMap.put("client_http_request_url", url);
+      if (correlationID != null) {
+        httpMap.put("correlation_id", String.join(",", correlationID));
+      }
+      if (requestSource != null) {
+        httpMap.put("request_source", String.join(",", requestSource));
+      }
       MDC.putCloseable("httpEvent", mapper.writeValueAsString(httpMap));
       log.info("");
       MDC.clear();
--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -54,6 +54,7 @@ echo Creating config map "$APP_NAME"-config-map
 oc create -n "$BUSINESS_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
  --from-literal=APP_LOG_LEVEL="$APP_LOG_LEVEL" \
  --from-literal=ENABLE_SPLUNK_LOG_HELPER="true" \
+ --from-literal=ENABLE_COMPRESSION="true" \
  --dry-run=client -o yaml | oc apply -f -
 
 echo Creating config map "$APP_NAME"-flb-sc-config-map
