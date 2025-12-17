bbc7c6052ef91d2ec4c444679a3b3e80a4c7b907
mightycox <j.acox@hotmail.com>
2025-12-16 17:27:27 -0800

    remove constant injection for now

--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/AssessmentRegistrationsRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/AssessmentRegistrationsRule.java
@@ -4,7 +4,6 @@ import ca.bc.gov.educ.api.ruleengine.dto.OptionalProgramRuleProcessor;
 import ca.bc.gov.educ.api.ruleengine.dto.RuleData;
 import ca.bc.gov.educ.api.ruleengine.dto.RuleProcessorData;
 import ca.bc.gov.educ.api.ruleengine.dto.StudentAssessment;
-import ca.bc.gov.educ.api.ruleengine.util.RuleEngineApiConstants;
 import ca.bc.gov.educ.api.ruleengine.util.RuleEngineApiUtils;
 import ca.bc.gov.educ.api.ruleengine.util.RuleProcessorRuleUtils;
 import lombok.AllArgsConstructor;
@@ -30,9 +29,6 @@ public class AssessmentRegistrationsRule implements Rule {
 	@Autowired
 	private RuleProcessorData ruleProcessorData;
 
-	@Autowired
-	RuleEngineApiConstants constants;
-
 	@Override
 	public RuleData fire() {
 
@@ -41,15 +37,15 @@ public class AssessmentRegistrationsRule implements Rule {
 		for (StudentAssessment studentAssessment : studentAssessmentList) {
 			String specialCase = StringUtils.isBlank(studentAssessment.getSpecialCase())? "" : studentAssessment.getSpecialCase();
 			String wroteFlag = StringUtils.isBlank(studentAssessment.getWroteFlag())? "" : studentAssessment.getWroteFlag();
-			if(constants.isEnableV2Changes()) {
+			/*if(constants.isEnableV2Changes()) {
 				if ("".compareTo(specialCase.trim()) == 0 && "N".compareTo(wroteFlag.trim()) == 0) {
 					studentAssessment.setProjected(true);
 				}
-			} else {
+			} else {*/
 				if ("".compareTo(specialCase.trim()) == 0 && "".compareTo(wroteFlag.trim()) == 0) {
 					studentAssessment.setProjected(true);
 				}
-			}
+			//}
 		}
 
 		ruleProcessorData.setExcludedAssessments(RuleProcessorRuleUtils.maintainExcludedAssessments(studentAssessmentList,ruleProcessorData.getExcludedAssessments(),ruleProcessorData.isProjected()));
--- api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/IncompleteAssessmentsRule.java
+++ api/src/main/java/ca/bc/gov/educ/api/ruleengine/rule/IncompleteAssessmentsRule.java
@@ -3,7 +3,6 @@ package ca.bc.gov.educ.api.ruleengine.rule;
 import ca.bc.gov.educ.api.ruleengine.dto.RuleData;
 import ca.bc.gov.educ.api.ruleengine.dto.RuleProcessorData;
 import ca.bc.gov.educ.api.ruleengine.dto.StudentAssessment;
-import ca.bc.gov.educ.api.ruleengine.util.RuleEngineApiConstants;
 import ca.bc.gov.educ.api.ruleengine.util.RuleProcessorRuleUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,8 +24,6 @@ public class IncompleteAssessmentsRule implements Rule {
 
     @Autowired
     private RuleProcessorData ruleProcessorData;
-    @Autowired
-    RuleEngineApiConstants constants;
 
     public RuleData fire() {
 
@@ -34,16 +31,16 @@ public class IncompleteAssessmentsRule implements Rule {
 
          for (StudentAssessment studentAssessment : studentAssessmentList) {
              String specialCase = StringUtils.isBlank(studentAssessment.getSpecialCase())? "" : studentAssessment.getSpecialCase();
-             if(constants.isEnableV2Changes()) {
+             /*if(false) {
                  if ("".compareTo(specialCase.trim()) == 0 && studentAssessment.isDidNotAttemptFlag()) {
                      studentAssessment.setNotCompleted(true);
                  }
-             } else {
+             } else {*/
                  String wroteFlag = StringUtils.isBlank(studentAssessment.getWroteFlag())? "" : studentAssessment.getWroteFlag();
                  if ("".compareTo(specialCase.trim()) == 0 && "N".compareTo(wroteFlag.trim()) == 0) {
                      studentAssessment.setNotCompleted(true);
                  }
-             }
+             //}
         }
 
         ruleProcessorData.setExcludedAssessments(RuleProcessorRuleUtils.maintainExcludedAssessments(studentAssessmentList,ruleProcessorData.getExcludedAssessments(),ruleProcessorData.isProjected()));
